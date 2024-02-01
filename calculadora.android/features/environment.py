c74c106fc1f8567489e4ba9a9762bedcd248314b
Thiago "F1ller" Oliveira <130257368+F1llerbRZ@users.noreply.github.com>
2024-02-01 12:21:54 -0300

    Add files via upload

--- calculadora.android/features/environment.py
+++ calculadora.android/features/environment.py
@@ -4,11 +4,11 @@ from app.application import Application
 def before_scenario(context, scenario):
     context.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub",
     desired_capabilities={"platformName": "Android",
-                                                            "automationName": "UIAutomator2",
-                                                            "appPackage": "com.google.android.calculator",
-                                                            "appActivity": "com.android.calculator2.Calculator",
-                                                            "platformVersion": "10.0"})
- 
+                            "automationName": "UIAutomator2",
+                            "appPackage": "com.google.android.calculator",
+                            "appActivity": "com.android.calculator2.Calculator",
+                            "platformVersion": "10.0"})
+
     context.driver.implicitly_wait(30)
     context.app = Application(context.driver)
 
